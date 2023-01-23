85c76239919af0e81bb5e2931727f990ea391795
rafaelcoelho <84815009+rircoelho@users.noreply.github.com>
2023-01-23 16:49:51 +0000

    add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,20 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.21"
+    val kotlinVersion: String = "1.7.21"
+
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.spring") version kotlinVersion
+    kotlin("plugin.jpa") version kotlinVersion
+    id("org.springframework.boot") version "3.0.0"
+    id("io.spring.dependency-management") version "1.1.0"
+
+    id("org.barfuin.gradle.jacocolog") version "1.2.4"
+    id("org.kordamp.gradle.detekt") version "0.45.0"
+    java
+    jacoco
+    id("org.jlleitschuh.gradle.ktlint") version "10.0.0"
+
     application
 }
 
@@ -10,9 +23,46 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven { url = uri("https://plugins.gradle.org/m2/") }
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
+    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("reflect"))
+    implementation("org.codehaus.janino:janino:3.1.9")
+    implementation("io.micrometer:micrometer-registry-statsd")
+    implementation("org.flywaydb:flyway-core:9.8.3")
+    implementation("com.vladmihalcea:hibernate-types-60:2.20.0")
+    implementation("org.springframework.kafka:spring-kafka")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.1")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-amqp")
+
+    runtimeOnly("org.postgresql:postgresql")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
+    // https://stackoverflow.com/questions/56387386/java-lang-noclassdeffounderror-org-junit-platform-launcher-testexecutionlistene
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.9.1")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("io.strikt:strikt-core:0.34.1")
+    testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0")
+    testImplementation("com.appmattus.fixture:fixture:1.2.0")
+    testImplementation("com.ninja-squad:springmockk:3.1.2")
+    testImplementation("com.squareup.okhttp3:okhttp:4.10.0")
+    testImplementation("com.squareup.okhttp3:mockwebserver:4.10.0")
+    testImplementation("com.tngtech.archunit:archunit-junit5:1.0.0")
+    testImplementation("org.springframework.amqp:spring-rabbit-test")
+    testImplementation("org.springframework.kafka:spring-kafka-test")
+
     testImplementation(kotlin("test"))
 }
 
